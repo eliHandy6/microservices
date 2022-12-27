959c32cb7fd99225b37cca97b49cc7766777988d
zack ogoma <zackogoma@gmail.com>
2022-12-27 21:29:00 +0300

    packaging config server

--- config-server/src/main/java/com/zackLabs/configserver/ConfigServer.java
+++ config-server/src/main/java/com/zackLabs/configserver/ConfigServer.java
@@ -3,9 +3,11 @@ package com.zackLabs.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
+@ComponentScan(basePackages = {"com.zackilabs", "com.zackLabs"})
 public class ConfigServer {
 
     public static void main(String args[]) {
--- config-server/src/main/java/com/zackLabs/configserver/securityConfig/SecurityConfig.java
+++ config-server/src/main/java/com/zackLabs/configserver/securityConfig/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.zackLabs.configserver.securityConfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -11,4 +12,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable();
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/actuator/**")
+                .antMatchers("/encrypt/**")
+                .antMatchers("/decrypt/**");
+        super.configure(web);
+    }
+
 }
