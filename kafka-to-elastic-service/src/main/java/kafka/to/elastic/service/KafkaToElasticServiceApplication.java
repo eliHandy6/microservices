2d191009ce1b37e922a73a9ee207d0f694d58ba5
zack ogoma <zackogoma@gmail.com>
2022-12-29 21:05:00 +0300

    kafka-elastic-service

---
+++ kafka-to-elastic-service/src/main/java/kafka/to/elastic/service/KafkaToElasticServiceApplication.java
@@ -0,0 +1,14 @@
+package kafka.to.elastic.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.zackLabs")
+public class KafkaToElasticServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaToElasticServiceApplication.class, args);
+    }
+}
+
